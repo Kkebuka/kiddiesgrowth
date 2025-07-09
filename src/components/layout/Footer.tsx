a70a8a3eede256e0f730a6cd1a42973820d46356
kkebuka <kkebuka@gmail.com>
2025-07-09 17:43:44 +0100

    minor change

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,13 +5,19 @@ import { FaWhatsapp, FaPhoneAlt } from "react-icons/fa";
 export default function Footer() {
   return (
     <div>
-      <footer className="footer justify-between sm:footer-horizontal bg-base-300 text-base-content p-10 ">
+      <footer className="footer  sm:footer-horizontal bg-base-300 text-base-content p-10 ">
         <nav>
           <h6 className="footer-title">Company</h6>
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <a className="link link-hover">Testimony</a>
+          {/* <a className="link link-hover">Press kit</a> */}
+        </nav>
+        <nav>
+          <h6 className="footer-title">Kiddies Community</h6>
+          <a className="link link-hover">Support a Child </a>
+          <a className="link link-hover">Blog</a>
+          <a className="link link-hover">Parenting Tips </a>
         </nav>
         <nav>
           <h6 className="footer-title">Contact Us</h6>
