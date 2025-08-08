771914e4b9319768e0a5306df8192f92fccd18c7
kkebuka <kkebuka@gmail.com>
2025-08-08 21:15:31 +0100

    minor fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
-import DashboardChart from "./pages/cartPage/DashboardChart";
-// import App from "./App.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      {/* <App /> */}
-      <DashboardChart />
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
