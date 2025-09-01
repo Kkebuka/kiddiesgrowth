bc2ee424c898ec116d3cd79a100d0c082f56f9e6
kkebuka <kkebuka@gmail.com>
2025-09-02 00:20:15 +0100

    setup user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,3 +24,13 @@ export const registerUser = async (req, resizeBy, next) => {
     next(err);
   }
 };
+
+export const getUserProfile = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+};
