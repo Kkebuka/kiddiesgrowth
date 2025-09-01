5cef862bafaa2485bcff22743167b8b8afd100fc
kkebuka <kkebuka@gmail.com>
2025-09-01 11:04:44 +0100

    configure controller

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
-import app from ".app.js";
 import connectDB from "./config/db.js";
+import morgan from "morgan";
 
 dotenv.config();
 
-const PORT = 5000 || process.env.PORT;
+const app = express();
+const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+connectDB();
 const startServer = async () => {
   try {
     await connectDB(
