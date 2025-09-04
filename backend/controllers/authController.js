125021c7c4c81cfb77be222ee0aeec9e78fbca67
kkebuka <kkebuka@gmail.com>
2025-09-04 19:38:46 +0100

    minor change

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,7 +15,7 @@ export const registerUser = async (req, res, next) => {
     if (userExists)
       return res.status(400).json({ message: "User already exists" });
 
-    const user = await User.create({ name, email, password });
+    const user = await User.create({ name, email, password, role });
 
     res.status(201).json({
       _id: user._id,
