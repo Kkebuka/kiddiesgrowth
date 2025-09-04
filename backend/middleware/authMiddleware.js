32c1b8575f323ac7200e702599fa47f7a588aaeb
kkebuka <kkebuka@gmail.com>
2025-09-04 18:40:26 +0100

    auth route added

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModels";
+
+export const protect = async (req, res, next) => {
+  let token;
+  if (
+    res.headers.authoriztion &&
+    req.headers.authoriztion.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authoriztion.split("")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (err) {
+      res.status(401).json({ message: "Not authorized, token failed" });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: "Not authorized, no token" });
+  }
+};
+
+export const admin = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(403).json({ message: "Admin only" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import morgan from "morgan";
 import router from "./routes/productRoutes.js";
+import authRoute from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 app.use("/products", router);
+app.use("/auth", authRoute);
 
 const startServer = async () => {
   await connectDB();
