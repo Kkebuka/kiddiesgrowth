f2854bed947beca241f69d7d90c8458b4c1a36fd
kkebuka <kkebuka@gmail.com>
2025-09-08 10:54:53 +0100

    minor change

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -4,4 +4,8 @@ const router = express.Router();
 
 router.get("/", getCategory);
 router.get("/:id", getCategoryById);
-router.post;
+router.post("/", createCategory);
+router.put("/:id", updateCategory);
+router.delete("/:id", deleteCategory);
+
+export default router;
