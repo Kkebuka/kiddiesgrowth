c3d40e1be055792830b12eb48dbb35f9fcd44487
kkebuka <kkebuka@gmail.com>
2025-09-08 11:40:21 +0100

    minor change

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
-
+import {
+  getCategory,
+  getCategoryById,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} from "../controllers/categoryController";
+import { protect, admin } from "../middleware/authMiddleware";
 const router = express.Router();
 
 router.get("/", getCategory);
 router.get("/:id", getCategoryById);
-router.post("/", createCategory);
-router.put("/:id", updateCategory);
-router.delete("/:id", deleteCategory);
+router.post("/", protect, admin, createCategory);
+router.put("/:id", protect, admin, updateCategory);
+router.delete("/:id", protect, admin, deleteCategory);
 
 export default router;
